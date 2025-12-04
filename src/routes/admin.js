cd8e0cd25e89a0dc5c1cf8ca1a2b568928f3afba
Maissen <maissen.developer500@gmail.com>
2025-12-04 13:14:45 +0100

    Fix error

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,10 +3,11 @@ import {
   getAllUsers
 } from "../controllers/adminController.js";
 import { isAdmin } from "../middlewares/roleMiddleware.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 // Get all users (admins, teachers, students) with all details (admin only)
-router.get("/users", isAdmin, getAllUsers);
+router.get("/users", verifyToken, isAdmin, getAllUsers);
 
 export default router;
